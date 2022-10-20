12601318849f2cc9b79abe2b9f8ddb8133465978
Daria Strekopytova <frau.strekopytova2010@yandex.ru>
2022-10-21 01:17:46 +0300

    3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет*/
+
+
+Console.WriteLine("Введи цифру");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+
+void checkday (int daynumber) 
+{
+  if (daynumber == 6 || daynumber == 7) 
+  {
+    Console.WriteLine("да");
+  }
+  else if (daynumber < 1 || daynumber > 7) 
+  {
+    Console.WriteLine("не подходит");
+  }
+  else Console.WriteLine("нет");
+}
+checkday(daynumber);
