ea5e75095998c4f2dbd7ad6e6df9d205dccf66a3
Daria Strekopytova <frau.strekopytova2010@yandex.ru>
2022-10-21 00:48:41 +0300

    1

---
+++ task1.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+int numberA = int.Parse(number);
+int first = numberA / 100;
+int second = numberA / 10;
+int result = second - first * 10;
+
+Console.WriteLine (result);
+
